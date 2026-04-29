d968ff9e3e799f7019d96de505f281c31300fe63
Alibi Galeyev <87616981+alad1nks@users.noreply.github.com>
2026-04-29 10:39:49 +0500

    Add color schemes (#17)

--- core/designsystem/src/commonMain/kotlin/com/alad1nks/dubovozki/feature/designsystem/theme/Theme.kt
+++ core/designsystem/src/commonMain/kotlin/com/alad1nks/dubovozki/feature/designsystem/theme/Theme.kt
@@ -2,22 +2,102 @@ package com.alad1nks.dubovozki.feature.designsystem.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
 
+private val LightColorScheme =
+    lightColorScheme(
+        primary = primaryLight,
+        onPrimary = onPrimaryLight,
+        primaryContainer = primaryContainerLight,
+        onPrimaryContainer = onPrimaryContainerLight,
+        secondary = secondaryLight,
+        onSecondary = onSecondaryLight,
+        secondaryContainer = secondaryContainerLight,
+        onSecondaryContainer = onSecondaryContainerLight,
+        tertiary = tertiaryLight,
+        onTertiary = onTertiaryLight,
+        tertiaryContainer = tertiaryContainerLight,
+        onTertiaryContainer = onTertiaryContainerLight,
+        error = errorLight,
+        onError = onErrorLight,
+        errorContainer = errorContainerLight,
+        onErrorContainer = onErrorContainerLight,
+        background = backgroundLight,
+        onBackground = onBackgroundLight,
+        surface = surfaceLight,
+        onSurface = onSurfaceLight,
+        surfaceVariant = surfaceVariantLight,
+        onSurfaceVariant = onSurfaceVariantLight,
+        outline = outlineLight,
+        outlineVariant = outlineVariantLight,
+        scrim = scrimLight,
+        inverseSurface = inverseSurfaceLight,
+        inverseOnSurface = inverseOnSurfaceLight,
+        inversePrimary = inversePrimaryLight,
+        surfaceDim = surfaceDimLight,
+        surfaceBright = surfaceBrightLight,
+        surfaceContainerLowest = surfaceContainerLowestLight,
+        surfaceContainerLow = surfaceContainerLowLight,
+        surfaceContainer = surfaceContainerLight,
+        surfaceContainerHigh = surfaceContainerHighLight,
+        surfaceContainerHighest = surfaceContainerHighestLight,
+    )
+
+private val DarkColorScheme =
+    darkColorScheme(
+        primary = primaryDark,
+        onPrimary = onPrimaryDark,
+        primaryContainer = primaryContainerDark,
+        onPrimaryContainer = onPrimaryContainerDark,
+        secondary = secondaryDark,
+        onSecondary = onSecondaryDark,
+        secondaryContainer = secondaryContainerDark,
+        onSecondaryContainer = onSecondaryContainerDark,
+        tertiary = tertiaryDark,
+        onTertiary = onTertiaryDark,
+        tertiaryContainer = tertiaryContainerDark,
+        onTertiaryContainer = onTertiaryContainerDark,
+        error = errorDark,
+        onError = onErrorDark,
+        errorContainer = errorContainerDark,
+        onErrorContainer = onErrorContainerDark,
+        background = backgroundDark,
+        onBackground = onBackgroundDark,
+        surface = surfaceDark,
+        onSurface = onSurfaceDark,
+        surfaceVariant = surfaceVariantDark,
+        onSurfaceVariant = onSurfaceVariantDark,
+        outline = outlineDark,
+        outlineVariant = outlineVariantDark,
+        scrim = scrimDark,
+        inverseSurface = inverseSurfaceDark,
+        inverseOnSurface = inverseOnSurfaceDark,
+        inversePrimary = inversePrimaryDark,
+        surfaceDim = surfaceDimDark,
+        surfaceBright = surfaceBrightDark,
+        surfaceContainerLowest = surfaceContainerLowestDark,
+        surfaceContainerLow = surfaceContainerLowDark,
+        surfaceContainer = surfaceContainerDark,
+        surfaceContainerHigh = surfaceContainerHighDark,
+        surfaceContainerHighest = surfaceContainerHighestDark,
+    )
+
 private val LightExtendedColorScheme =
     ExtendedColorScheme(
-        odintsovo = Color.Black,
-        slavyanskyBulvar = light_green,
-        molodyozhnaya = light_blue,
+        busScheduleStationOdintsovo = Color.Black,
+        busScheduleStationSlavyanskyBulvar = greenLight,
+        busScheduleStationMolodyozhnaya = blueLight,
     )
 
 private val DarkExtendedColorScheme =
     ExtendedColorScheme(
-        odintsovo = Color.White,
-        slavyanskyBulvar = dark_green,
-        molodyozhnaya = dark_blue,
+        busScheduleStationOdintsovo = Color.White,
+        busScheduleStationSlavyanskyBulvar = greenDark,
+        busScheduleStationMolodyozhnaya = blueDark,
     )
 
 @Composable
@@ -25,14 +105,18 @@ fun AppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit,
 ) {
-    // TODO Add full dark theme and uncomment
-    // val extendedColorScheme = if (darkTheme) DarkExtendedColorScheme else LightExtendedColorScheme
-    val extendedColorScheme = LightExtendedColorScheme
+    val colorScheme =
+        when {
+            darkTheme -> DarkColorScheme
+            else -> LightColorScheme
+        }
+    val extendedColorScheme = if (darkTheme) DarkExtendedColorScheme else LightExtendedColorScheme
 
     CompositionLocalProvider(
         LocalExtendedColorScheme provides extendedColorScheme,
     ) {
         MaterialTheme(
+            colorScheme = colorScheme,
             content = content,
         )
     }
