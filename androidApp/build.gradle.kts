7847d1ad6f78a738726942835910b0afa8dc91e1
Alibi Galeyev <87616981+alad1nks@users.noreply.github.com>
2026-05-09 12:26:25 +0500

    Release 2.1 (#39)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.alad1nks.dubovozki"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 5
-        versionName = "2.0"
+        versionCode = 6
+        versionName = "2.1"
     }
     packaging {
         resources {
